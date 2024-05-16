19bee4f47d51d56caa64e209e13017ae7a298b32
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-05-16 18:36:05 -0400

    test: test for 2 + 2

--- src/tests/calculator.test.js
+++ src/tests/calculator.test.js
@@ -3,3 +3,7 @@ import calculator from '../modules/calculator';
 test('calculate 1 + 1 = 2', () => {
   expect(calculator.add(1, 1)).toBe(2);
 });
+
+test('calculate 2 + 2 = 4', () => {
+  expect(calculator.add(2, 2)).toBe(4);
+});
