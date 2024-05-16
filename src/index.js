e964aeb1d53eab1b9226e8edae3411ac40edf6ef
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-05-16 09:04:21 -0400

    first test

---
+++ src/index.js
@@ -0,0 +1,5 @@
+function capitalize(string) {
+  return 'String';
+}
+
+export default capitalize;
---
+++ src/index.test.js
@@ -0,0 +1,5 @@
+import capitalize from '.';
+
+test('capitalizes string to String', () => {
+  expect(capitalize('string')).toBe('String');
+});
