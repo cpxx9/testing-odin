e6a5dfa0c3fb773a2cc9e4fc56e386ad0e057786
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-05-16 20:54:40 -0400

    test: separated calculator tests with describe

--- src/modules/calculator.js
+++ src/modules/calculator.js
@@ -2,6 +2,12 @@ const calculator = {
   add(x, y) {
     return x + y;
   },
+  sub(x, y) {
+    return x - y;
+  },
+  mul(x, y) {
+    return x * y;
+  },
 };
 
 export default calculator;
--- src/tests/calculator.test.js
+++ src/tests/calculator.test.js
@@ -1,13 +1,39 @@
 import calculator from '../modules/calculator';
 
-test('calculate 1 + 1 = 2', () => {
-  expect(calculator.add(1, 1)).toBe(2);
+describe('add method tests', () => {
+  test('calculate 1 + 1 = 2', () => {
+    expect(calculator.add(1, 1)).toBe(2);
+  });
+
+  test('calculate 2 + 2 = 4', () => {
+    expect(calculator.add(2, 2)).toBe(4);
+  });
+
+  test('calculate 5.2 + 11.7 = 16.9', () => {
+    expect(calculator.add(5.2, 11.7)).toBe(16.9);
+  });
 });
 
-test('calculate 2 + 2 = 4', () => {
-  expect(calculator.add(2, 2)).toBe(4);
+describe('subtraction method tests', () => {
+  test('calculate 5 - 3 = 2', () => {
+    expect(calculator.sub(5, 2)).toBe(3);
+  });
+
+  test('calculate 7 - 3 = 4', () => {
+    expect(calculator.sub(7, 3)).toBe(4);
+  });
+
+  test('calculate 7.6 - 3.2 = 4.4', () => {
+    expect(calculator.sub(7.6, 3.2)).toBeCloseTo(4.4);
+  });
 });
 
-test('calculate 5.2 + 11.7 = 16.9', () => {
-  expect(calculator.add(5.2, 11.7)).toBe(16.9);
+describe('multiplication method tests', () => {
+  test('calculate 7 * 3 = 21', () => {
+    expect(calculator.mul(7, 3)).toBeCloseTo(21);
+  });
+
+  test('calculate 7 * 4 = 28', () => {
+    expect(calculator.mul(7, 4)).toBeCloseTo(28);
+  });
 });
