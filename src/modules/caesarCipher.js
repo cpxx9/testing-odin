3220661edf7b68cfdbd0b50bba88afc9af5b7041
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-05-17 17:00:00 -0400

    working with punctuation

--- src/modules/caesarCipher.js
+++ src/modules/caesarCipher.js
@@ -1,8 +1,16 @@
 function caesarCipher(string, key = 1) {
   const stringArr = Array.from(string);
   let newString = '';
+  let shiftKey = key;
   stringArr.forEach((char, index) => {
-    newString += String.fromCharCode(char.charCodeAt() + key);
+    if (
+      (char.charCodeAt() > 64 && char.charCodeAt() < 91) ||
+      (char.charCodeAt() > 96 && char.charCodeAt() < 123)
+    ) {
+      newString += String.fromCharCode(char.charCodeAt() + shiftKey);
+    } else {
+      newString += char;
+    }
   });
   return newString;
 }
