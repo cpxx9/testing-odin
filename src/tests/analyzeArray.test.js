501bb5b84ef57e8e912ccdc1c1ba010d9a0becde
CPxx9 <127376939+cpxx9@users.noreply.github.com>
2024-05-18 15:16:52 -0400

    test: work with different arrays

---
+++ src/tests/analyzeArray.test.js
@@ -0,0 +1,19 @@
+import analyzeArray from '../modules/analyzeArray';
+
+test('return array details', () => {
+  expect(analyzeArray([0, 1, 2, 3, 4])).toBe({
+    average: 2,
+    min: 0,
+    max: 4,
+    length: 5,
+  });
+});
+
+test('return array details', () => {
+  expect(analyzeArray([7, 2, 5, 1, 4])).toBe({
+    average: 4,
+    min: 1,
+    max: 7,
+    length: 5,
+  });
+});
