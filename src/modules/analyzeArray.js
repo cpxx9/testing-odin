64d3b99ea4433b3da9666710e9c509bf87b478b2
CPxx9 <127376939+cpxx9@users.noreply.github.com>
2024-05-18 15:48:05 -0400

    feat: analyzeArray returns min,max,avg,length

--- src/modules/analyzeArray.js
+++ src/modules/analyzeArray.js
@@ -1,3 +1,13 @@
-function analyzeArray(arr) {}
+function analyzeArray(arr) {
+  const sortedArr = arr.sort();
+  const arrInfo = {
+    average: sortedArr[Math.floor(sortedArr.length / 2)],
+    length: sortedArr.length,
+    min: sortedArr[0],
+    max: sortedArr[sortedArr.length - 1],
+  };
+
+  return arrInfo;
+}
 
 export default analyzeArray;
--- src/tests/analyzeArray.test.js
+++ src/tests/analyzeArray.test.js
@@ -1,7 +1,7 @@
 import analyzeArray from '../modules/analyzeArray';
 
 test('return array details', () => {
-  expect(analyzeArray([0, 1, 2, 3, 4])).toBe({
+  expect(analyzeArray([0, 1, 2, 3, 4])).toEqual({
     average: 2,
     min: 0,
     max: 4,
@@ -10,7 +10,7 @@ test('return array details', () => {
 });
 
 test('return array details', () => {
-  expect(analyzeArray([7, 2, 5, 1, 4])).toBe({
+  expect(analyzeArray([7, 2, 5, 1, 4])).toEqual({
     average: 4,
     min: 1,
     max: 7,
